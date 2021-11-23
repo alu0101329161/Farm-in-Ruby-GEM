d84d9acb62e14e46edff2f94a87eaefea59e1cbe
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-23 18:29:04 +0000

    Fallo en prueba de granja::Funcion

--- spec/granja_spec.rb
+++ spec/granja_spec.rb
@@ -2,4 +2,7 @@ RSpec.describe Granja do
   it "Tiene un número de version, usando la sintaxis semántica 0.1.0" do
     expect(Granja::VERSION).not_to be nil
   end
+  describe Granja::Funcion do 
+  end
+
 end
