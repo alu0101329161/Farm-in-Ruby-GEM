c1770c6b5801351529df165f85a231d6c24af0d8
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-23 21:34:32 +0000

    Prueba de to_s fallada

--- spec/granja_spec.rb
+++ spec/granja_spec.rb
@@ -27,6 +27,14 @@ RSpec.describe Granja do
       expect(Granja::Funcion.new("","","10").reproduccion).to eq("10")
 
     end
+    it "Tiene un metodo para poder imprimir" do
+      expect(Granja::Funcion.new("Saludables","Buena","10").to_s).to eq("Las condiciones de vida de los animales son: Saludable \n La calidad de los cuidados es Buena \n y el nivel de reproduccion es 10")
+      expect(Granja::Funcion.new("Saludables","Mala","5").to_s).to eq("Las condiciones de vida de los animales son: Saludable \n La calidad de los cuidados es Mala \n y el nivel de reproduccion es 5")
+      expect(Granja::Funcion.new("Poca","3","2").to_s).to eq("Las condiciones de vida de los animales son: Poca \n La calidad de los cuidados es 3 \n y el nivel de reproduccion es 2")
+      expect(Granja::Funcion.new("Mala","1","5").to_s).to eq("Las condiciones de vida de los animales son: Mala \n La calidad de los cuidados es 1 \n y el nivel de reproduccion es 5")
+
+    end
+
  
   end
 
