b94ad9fcf9d08704af4dccd2e1729806cf3cfe69
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-23 21:25:18 +0000

    Prueba de atributo reproduccion de animal pasada

--- lib/granja.rb
+++ lib/granja.rb
@@ -3,11 +3,12 @@ require "granja/version"
 module Granja
   class Error < StandardError; end
   class Funcion
-    attr_reader :condicion_de_vida, :cuidado
+    attr_reader :condicion_de_vida, :cuidado, :reproduccion
     #Constructor de la clase Funcion
-    def initialize(condicion_vida = "", cuidado = "")
+    def initialize(condicion_vida = "", cuidado = "", reproduccion = "")
       @condicion_de_vida = condicion_vida
       @cuidado = cuidado
+      @reproduccion = reproduccion
     end 
 
 
