a46a4f31aaf2b34f99d8f3991cd7275959239dc8
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-23 18:43:42 +0000

    Arreglo clase funcion error

--- spec/granja_spec.rb
+++ spec/granja_spec.rb
@@ -2,7 +2,10 @@ RSpec.describe Granja do
   it "Tiene un número de version, usando la sintaxis semántica 0.1.0" do
     expect(Granja::VERSION).not_to be nil
   end
-  describe Granja::Funcion do 
+  describe Granja::Funcion do
+    it "Tiene una clase para almacenar las funcionalidades" do
+      expect(Granja::Funcion.new()).not_to eq(nil)
+    end 
   end
 
 end
