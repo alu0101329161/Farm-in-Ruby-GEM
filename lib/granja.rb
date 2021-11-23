fb688fd874b57b98a5cbcb9ce586b3390832f726
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-23 18:23:08 +0000

    Añadida version de la gema

--- lib/granja.rb
+++ lib/granja.rb
@@ -2,5 +2,4 @@ require "granja/version"
 
 module Granja
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/granja_spec.rb
+++ spec/granja_spec.rb
@@ -1,6 +1,5 @@
 RSpec.describe Granja do
-  it "has a version number" do
+  it "Tiene un número de version, usando la sintaxis semántica 0.1.0" do
     expect(Granja::VERSION).not_to be nil
   end
-
 end
