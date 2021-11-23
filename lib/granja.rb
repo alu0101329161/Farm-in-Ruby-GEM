148158650d896d293e48e29b9fd26e592a90514b
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-23 22:29:36 +0000

    Prueba to_s pasada

--- lib/granja.rb
+++ lib/granja.rb
@@ -25,6 +25,9 @@ module Granja
       @tipo = tipo
       @descripcion = descripcion
     end
-
+    #metodo para imprimir
+    def to_s
+      "la identificacion de la granja es: #{@identificacion} \n el nombre es: #{@nombre} \n , el tipo es: #{@tipo} \n y la descripcion es: #{@descripcion}"
+    end
   end
 end
