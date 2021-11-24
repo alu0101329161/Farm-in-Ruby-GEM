1db7baaa6b4a9d125d4834e1cfd204de547bfc45
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-24 17:26:30 +0000

    Documentacion generada con Yard

--- lib/granja/datos.rb
+++ lib/granja/datos.rb
@@ -1,13 +1,20 @@
+ # Guardar los datos de la granja
   class Datos
+    # @overload getter de los atributos
     attr_reader :identificacion, :nombre, :tipo, :descripcion
-    #Constructor de la clase Funcion
+
+    # Constructor de la clase datos
+    # @note se aceptan 4 parametros de cualquier tipo
+    # @return inicializa atributos
     def initialize(identificacion = "", nombre = "", tipo = "", descripcion = "")
       @identificacion = identificacion
       @nombre = nombre
       @tipo = tipo
       @descripcion = descripcion
     end
-    #metodo para imprimir
+
+    # Metodo para convertir en string
+    # @return String
     def to_s
       "la identificacion de la granja es: #{@identificacion} \n el nombre es: #{@nombre} \n , el tipo es: #{@tipo} \n y la descripcion es: #{@descripcion}"
     end
--- lib/granja/funcion.rb
+++ lib/granja/funcion.rb
@@ -1,12 +1,20 @@
+# Clase para almacenar funcionalidades de la granja
 class Funcion
+
+  # @overload ggeter de los atributos
   attr_reader :condicion_de_vida, :cuidado, :reproduccion
-  #Constructor de la clase Funcion
+  
+  # Metodo para inicializar atrubutos de la clase Funcion
+  # @note acepta 3 parametro de cualquier tipo
+  # @return atributos de la clase
   def initialize(condicion_vida = "", cuidado = "", reproduccion = "")
     @condicion_de_vida = condicion_vida
     @cuidado = cuidado
     @reproduccion = reproduccion
    end
-
+ 
+   # Metodo para impirmir las variables de instancia
+   # @return String
    def to_s
      "Las condiciones de vida de los animales son: #{@condicion_de_vida} \n La calidad de los cuidados es #{@cuidado} \n y el nivel de reproduccion es #{@reproduccion}"
    end
