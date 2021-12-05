f3050e6c8a5505b9d969717e3538239786a11e85
alu0101329161 <alu0101329161@ull.edu.es>
2021-12-05 18:51:17 +0000

    Prueba atributos clase Ganado

--- lib/granja/ganado.rb
+++ lib/granja/ganado.rb
@@ -1,7 +1,14 @@
 
 class Ganado < Animal
 
+    attr_reader :raza, :aprovechamiento, :alimentacion
+
     def initialize(raza = "", apro = "", ali = "", iden = 0, edad = 0, sexo = "", peso = 0)
+
+        super(iden,edad,sexo,peso)
+        @raza = raza
+        @aprovechamiento = apro
+        @alimentacion = ali
 
     end
 
--- spec/ganado_spec.rb
+++ spec/ganado_spec.rb
@@ -1,9 +1,34 @@
 RSpec.describe Granja do
     describe Ganado do
 
+        before :each do
+          @test1 = Ganado.new("bovino","carne","herbívoro")
+          @test2 = Ganado.new("porcino","piel","omnívoro")
+          @test3 = Ganado.new("ovino","leche","herbívoro")
+          @test4 = Ganado.new("caprino","carne","herbívoro")
+        end
+
         context "Atributos de la clase Ganado" do
             it "Tiene una clase para representar ganado" do
                 expect(Ganado.new()).not_to eq(nil)
+            end
+            it "Tiene un atributo para la raza" do
+                expect(@test1.raza).to eq("bovino")
+                expect(@test2.raza).to eq("porcino")
+                expect(@test3.raza).to eq("ovino")
+                expect(@test4.raza).to eq("caprino")
+            end
+            it "Tiene un atributo para el tipo de aprovechamiento " do
+                expect(@test1.aprovechamiento).to eq("carne")
+                expect(@test2.aprovechamiento).to eq("piel")
+                expect(@test3.aprovechamiento).to eq("leche")
+                expect(@test4.aprovechamiento).to eq("carne")
+            end
+            it "Tiene un atributo para su tipo de alimentacion " do
+                expect(@test1.alimentacion).to eq("herbívoro")
+                expect(@test2.alimentacion).to eq("omnívoro")
+                expect(@test3.alimentacion).to eq("herbívoro")
+                expect(@test4.alimentacion).to eq("herbívoro")
             end
         end
 
