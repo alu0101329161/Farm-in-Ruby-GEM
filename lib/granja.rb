a00a62629e74987687e24657446eaf6727beeec8
alu0101329161 <alu0101329161@ull.edu.es>
2021-12-05 17:44:41 +0000

    EPasada prueba lase animal

--- lib/granja.rb
+++ lib/granja.rb
@@ -1,6 +1,7 @@
 require "granja/version"
 require "granja/datos"
 require "granja/funcion"
+require "granja/animal"
 
 module Granja
   class Error < StandardError; end
---
+++ lib/granja/animal.rb
@@ -0,0 +1,6 @@
+class Animal
+
+    def initialize(iden = 0, edad = 0, sexo = "", peso = 0)
+    
+    end
+end
